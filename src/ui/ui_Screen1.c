3e0532ae0014808d9dd9242be51bcaaa41866565
Lukas Jezny <ljezny@gmail.com>
2024-08-01 16:17:05 +0200

    Chart design

--- src/ui/ui_Screen1.c
+++ src/ui/ui_Screen1.c
@@ -570,7 +570,7 @@ lv_obj_set_height( ui_Chart1, lv_pct(100));
 lv_obj_set_align( ui_Chart1, LV_ALIGN_CENTER );
 lv_chart_set_type( ui_Chart1, LV_CHART_TYPE_LINE);
 lv_chart_set_point_count( ui_Chart1, 24);
-lv_chart_set_div_line_count( ui_Chart1, 10, 5);
+lv_chart_set_div_line_count( ui_Chart1, 5, 4);
 lv_chart_set_axis_tick( ui_Chart1, LV_CHART_AXIS_PRIMARY_X, 10, 5, 10, 5, true, 20);
 lv_chart_set_axis_tick( ui_Chart1, LV_CHART_AXIS_PRIMARY_Y, 10, 5, 5, 2, true, 50);
 lv_chart_set_axis_tick( ui_Chart1, LV_CHART_AXIS_SECONDARY_Y, 10, 5, 5, 2, true, 25);
@@ -580,9 +580,14 @@ lv_chart_set_ext_y_array(ui_Chart1, ui_Chart1_series_1, ui_Chart1_series_1_array
 lv_chart_series_t* ui_Chart1_series_2 = lv_chart_add_series(ui_Chart1, lv_color_hex(0x0A1BED), LV_CHART_AXIS_SECONDARY_Y);
 static lv_coord_t ui_Chart1_series_2_array[] = { 100,100,100,80,60,60,40,40,20,10,100,100,100,80,60,60,40,40,20,10,100,100,100,80,60,60,40,40,20,10 };
 lv_chart_set_ext_y_array(ui_Chart1, ui_Chart1_series_2, ui_Chart1_series_2_array);
+lv_obj_set_style_border_color(ui_Chart1, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT );
+lv_obj_set_style_border_opa(ui_Chart1, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
+lv_obj_set_style_border_width(ui_Chart1, 0, LV_PART_MAIN| LV_STATE_DEFAULT);
 
+lv_obj_set_style_line_width(ui_Chart1, 2, LV_PART_ITEMS| LV_STATE_DEFAULT);
+lv_obj_set_style_line_rounded(ui_Chart1, true, LV_PART_ITEMS| LV_STATE_DEFAULT);
 
+lv_obj_set_style_size(ui_Chart1, 0, LV_PART_INDICATOR| LV_STATE_DEFAULT);
 
-lv_obj_set_style_text_font(ui_Chart1, &ui_font_RobotoMonoSmall, LV_PART_TICKS| LV_STATE_DEFAULT);
 
 }
