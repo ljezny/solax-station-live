482245e89ae5285af39612a0dc00d7ac968546df
Lukas Jezny <ljezny@gmail.com>
2024-08-25 14:26:45 +0200

    Fixes day sell

--- src/Inverters/Goodwe/GoodweDongleAPI.hpp
+++ src/Inverters/Goodwe/GoodweDongleAPI.hpp
@@ -153,7 +153,7 @@ private:
                             inverterData.loadToday = readUInt16(packetBuffer, 105) / 10;
                             inverterData.batteryChargedToday = readUInt16(packetBuffer, 108) / 10;
                             inverterData.batteryDischargedToday = readUInt16(packetBuffer, 111) / 10;
-                            inverterData.gridBuyToday = readUInt16(packetBuffer, 101) / 10;
+                            inverterData.gridBuyToday = readUInt16(packetBuffer, 102) / 10;
                             inverterData.gridSellToday = readUInt16(packetBuffer, 99) / 10;                            
                         }
                     }
