06ff34f0774cf7d644d655e43b0eb28d5a953c92
Lukas Jezny <ljezny@gmail.com>
2024-09-05 12:11:28 +0200

    Support for new inverter of solax

--- src/Inverters/Solax/SolaxDongleAPI.hpp
+++ src/Inverters/Solax/SolaxDongleAPI.hpp
@@ -42,21 +42,28 @@ public:
                         inverterData.soc = doc["Data"][103].as<int>();
                         inverterData.pvToday = doc["Data"][70].as<uint16_t>() / 10.0;
                         inverterData.pvTotal = ((doc["Data"][69].as<uint32_t>() << 16) + doc["Data"][68].as<uint16_t>()) / 10.0;
-                        //inverterData.loadToday = ((doc["Data"][89].as<uint32_t>() << 16) + doc["Data"][86].as<uint16_t>()) / 100.0; 
                         inverterData.feedInPower = read16BitSigned(doc["Data"][34].as<uint16_t>());
                         inverterData.gridSellToday = doc["Data"][90].as<uint16_t>() / 100.0; 
                         inverterData.gridBuyToday = doc["Data"][92].as<uint16_t>() / 100.0;
-                        //inverterData.gridSellTotal = ((doc["Data"][87].as<uint32_t>() << 16) + doc["Data"][86].as<uint16_t>()) / 100.0; 
-                        //inverterData.gridBuyTotal = ((doc["Data"][89].as<uint32_t>() << 16) + doc["Data"][88].as<uint16_t>()) / 100.0; 
                         inverterData.batteryChargedToday = doc["Data"][79].as<uint16_t>() / 10.0;
                         inverterData.batteryDischargedToday = doc["Data"][78].as<uint16_t>() / 10.0;
-                        //inverterData.batteryChargedTotal = ((doc["Data"][77].as<uint32_t>() << 16) + doc["Data"][76].as<uint16_t>()) / 10.0; 
-                        //inverterData.batteryDischargedTotal = ((doc["Data"][75].as<uint32_t>() << 16) + doc["Data"][74].as<uint16_t>()) / 10.0; 
-                        //inverterData.loadTotal = inverterData.pvTotal + inverterData.gridBuyTotal;
                         inverterData.loadToday = inverterData.pvToday + inverterData.gridBuyToday;
                         inverterData.sn = sn;
-
-                        //wallboxData.power = doc["Data"][11].as<int>();
+                    } if(doc["type"].as<int>() == 16) { //X3-MIC/PRO-G2 https://github.com/simatec/ioBroker.solax/blob/master/lib/inverterData.js
+                        inverterData.status = DONGLE_STATUS_OK;
+                        inverterData.millis = millis();
+                        inverterData.pv1Power = doc["Data"][15].as<int>();
+                        inverterData.pv2Power = doc["Data"][16].as<int>() 
+                                                + doc["Data"][17].as<int>();
+                        inverterData.L1Power = doc["Data"][6].as<int>();
+                        inverterData.L2Power = doc["Data"][7].as<int>();
+                        inverterData.L3Power = doc["Data"][8].as<int>();
+                        inverterData.inverterPower = doc["Data"][78].as<int>();
+                        inverterData.pvToday = doc["Data"][24].as<uint16_t>() / 10.0;
+                        inverterData.pvTotal = doc["Data"][22].as<uint16_t>() / 10.0;
+                        inverterData.feedInPower = read16BitSigned(doc["Data"][74].as<uint16_t>());
+                        inverterData.gridSellToday = doc["Data"][74].as<uint16_t>() / 100.0; 
+                        inverterData.sn = sn;
                     } else if(doc["type"].as<int>() == 1) { //wallbox
                         //wallboxData.power = doc["Data"][11].as<int>();
                         inverterData.status = DONGLE_STATUS_UNSUPPORTED_DONGLE;
