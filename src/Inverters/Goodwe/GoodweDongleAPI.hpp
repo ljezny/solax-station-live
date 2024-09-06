ebd6e81f841a6f78ab26cb1772fcb8f9bfa97e71
Lukas Jezny <ljezny@gmail.com>
2024-09-06 16:54:26 +0200

    Fixes Goodwe grid buy and sell

--- src/Inverters/Goodwe/GoodweDongleAPI.hpp
+++ src/Inverters/Goodwe/GoodweDongleAPI.hpp
@@ -127,72 +127,90 @@ private:
             log_d("Connected.");
             int PACKET_SIZE = 259; // WHY?
             byte packetBuffer[PACKET_SIZE];
-            
-            sendRunningDataRequestPacket();
-            if (awaitPacket(3000))
-            {
-                int len = udp.read(packetBuffer, PACKET_SIZE);
-                if (len > 0)
-                {
-                    unsigned c = crc16(packetBuffer + 2, len - 2, 0x8005, 0xFFFF, 0, true, true);
-                    if (c == 0)
-                    {
-                        if (len == 7 + 2 * 125)
-                        {
-                            inverterData.status = DONGLE_STATUS_OK;
-                            inverterData.millis = millis();
-                            inverterData.pv1Power = readUInt32(packetBuffer, 5);
-                            inverterData.pv2Power = readUInt32(packetBuffer, 9);
-                            inverterData.pv3Power = readUInt32(packetBuffer, 13);
-                            inverterData.pv4Power = readUInt32(packetBuffer, 17); 
-                            inverterData.inverterPower = readInt16(packetBuffer, 38);
-                            inverterData.batteryPower -= readInt16(packetBuffer, 83); //TODO: maybe sign readuw(packetBuffer, 84);
-                            // _ac = readsw(packetBuffer, 40);
-                            inverterData.L1Power = readInt16(packetBuffer, 25);// - readInt16(packetBuffer, 64) + readInt16(packetBuffer, 50);
-                            inverterData.L2Power = readInt16(packetBuffer, 30);// - readInt16(packetBuffer, 66) + readInt16(packetBuffer, 56);
-                            inverterData.L3Power = readInt16(packetBuffer, 35);// - readInt16(packetBuffer, 68) + readInt16(packetBuffer, 62);
-                            inverterData.feedInPower = 
-                                readInt16(packetBuffer, 25) 
-                                + readInt16(packetBuffer, 30) 
-                                + readInt16(packetBuffer, 35)
-                                - readInt16(packetBuffer, 64) - readInt16(packetBuffer, 50)
-                                - readInt16(packetBuffer, 66) - readInt16(packetBuffer, 56)
-                                - readInt16(packetBuffer, 68) - readInt16(packetBuffer, 62);
-                            inverterData.loadPower = readInt16(packetBuffer, 72) + readInt16(packetBuffer, 70);
-                            inverterData.inverterTemperature = readInt16(packetBuffer, 74) / 10;
-                            inverterData.pvTotal = readUInt32(packetBuffer, 91) / 10.0;
-                            inverterData.pvToday = readUInt32(packetBuffer, 93) / 10.0;
-                            inverterData.loadToday = readUInt16(packetBuffer, 105) / 10.0;
-                            inverterData.loadTotal = readUInt32(packetBuffer, 103) / 10.0;
-                            inverterData.batteryChargedToday = readUInt16(packetBuffer, 108) / 10.0;
-                            inverterData.batteryDischargedToday = readUInt16(packetBuffer, 111) / 10.0;
-                            inverterData.gridBuyToday = readUInt16(packetBuffer, 102) / 10.0;
-                            inverterData.gridSellToday = readUInt16(packetBuffer, 99) / 10.0 - inverterData.loadToday;       
-                            inverterData.gridBuyTotal = readUInt32(packetBuffer, 100) / 10.0;
-                            inverterData.gridSellTotal = readUInt32(packetBuffer, 95) / 10.0;
-                            inverterData.sn = sn;                     
 
-                            //this is a hack - Goodwe returns incorrect day values for grid sell/buy
-                            //so count it manually from total values
-                            int day = (readUInt16(packetBuffer, 1) >> 8) & 0xFF;
-                            log_d("Day: %d", day);
-                            if(this->day != day) {
-                                log_d("Day changed, resetting counters");
-                                this->day = day;
-                                gridBuyTotal = inverterData.gridBuyTotal;
-                                gridSellTotal = inverterData.gridSellTotal;
-                                loadTotal = inverterData.loadTotal;
+            for (int i = 0; i < 3; i++)
+            {
+                sendRunningDataRequestPacket();
+                if (awaitPacket(3000))
+                {
+                    int len = udp.read(packetBuffer, PACKET_SIZE);
+                    if (len > 0)
+                    {
+                        unsigned c = crc16(packetBuffer + 2, len - 2, 0x8005, 0xFFFF, 0, true, true);
+                        if (c == 0)
+                        {
+                            if (len == 7 + 2 * 125)
+                            {
+                                inverterData.status = DONGLE_STATUS_OK;
+                                inverterData.millis = millis();
+                                inverterData.pv1Power = readUInt32(packetBuffer, 5);
+                                inverterData.pv2Power = readUInt32(packetBuffer, 9);
+                                inverterData.pv3Power = readUInt32(packetBuffer, 13);
+                                inverterData.pv4Power = readUInt32(packetBuffer, 17);
+                                inverterData.inverterPower = readInt16(packetBuffer, 38);
+                                inverterData.batteryPower -= readInt16(packetBuffer, 83); // TODO: maybe sign readuw(packetBuffer, 84);
+                                // _ac = readsw(packetBuffer, 40);
+                                inverterData.L1Power = readInt16(packetBuffer, 25); // - readInt16(packetBuffer, 64) + readInt16(packetBuffer, 50);
+                                inverterData.L2Power = readInt16(packetBuffer, 30); // - readInt16(packetBuffer, 66) + readInt16(packetBuffer, 56);
+                                inverterData.L3Power = readInt16(packetBuffer, 35); // - readInt16(packetBuffer, 68) + readInt16(packetBuffer, 62);
+                                inverterData.feedInPower =
+                                    readInt16(packetBuffer, 25) + readInt16(packetBuffer, 30) + readInt16(packetBuffer, 35) - readInt16(packetBuffer, 64) - readInt16(packetBuffer, 50) - readInt16(packetBuffer, 66) - readInt16(packetBuffer, 56) - readInt16(packetBuffer, 68) - readInt16(packetBuffer, 62);
+                                inverterData.loadPower = readInt16(packetBuffer, 72) + readInt16(packetBuffer, 70);
+                                inverterData.inverterTemperature = readInt16(packetBuffer, 74) / 10;
+                                inverterData.pvTotal = readUInt32(packetBuffer, 91) / 10.0;
+                                inverterData.pvToday = readUInt32(packetBuffer, 93) / 10.0;
+                                inverterData.loadToday = readUInt16(packetBuffer, 105) / 10.0;
+                                inverterData.loadTotal = readUInt32(packetBuffer, 103) / 10.0;
+                                inverterData.batteryChargedToday = readUInt16(packetBuffer, 108) / 10.0;
+                                inverterData.batteryDischargedToday = readUInt16(packetBuffer, 111) / 10.0;
+                                inverterData.gridBuyToday = readUInt16(packetBuffer, 102) / 10.0;
+                                inverterData.gridSellToday = readUInt16(packetBuffer, 99) / 10.0 - inverterData.loadToday;
+                                inverterData.gridBuyTotal = readUInt32(packetBuffer, 100) / 10.0;
+                                inverterData.gridSellTotal = readUInt32(packetBuffer, 95) / 10.0;
+                                inverterData.sn = sn;
+                                logInverterData(inverterData);
+                                // this is a hack - Goodwe returns incorrect day values for grid sell/buy
+                                // so count it manually from total values
+                                // int day = (readUInt16(packetBuffer, 1) >> 8) & 0xFF;
+                                // log_d("Day: %d", day);
+                                // if (this->day != day)
+                                // {
+                                //     log_d("Day changed, resetting counters");
+                                //     this->day = day;
+                                //     gridBuyTotal = inverterData.gridBuyTotal;
+                                //     gridSellTotal = inverterData.gridSellTotal;
+                                //     loadTotal = inverterData.loadTotal;
+                                // }
+                                // log_d("Grid buy total: %f", gridBuyTotal);
+                                // log_d("Grid sell total: %f", gridSellTotal);
+                                //inverterData.gridBuyToday = inverterData.gridBuyTotal - gridBuyTotal;
+                                //inverterData.gridSellToday = inverterData.gridSellTotal - gridSellTotal - (inverterData.loadTotal - loadTotal);
+                                
+                                inverterData.gridBuyToday = inverterData.pvToday + inverterData.batteryDischargedToday - inverterData.batteryChargedToday - inverterData.gridSellToday - inverterData.loadToday;
+
+                                break;
                             }
-                            log_d("Grid buy total: %f", gridBuyTotal);
-                            log_d("Grid sell total: %f", gridSellTotal);
-                            inverterData.gridBuyToday = inverterData.gridBuyTotal - gridBuyTotal;
-                            inverterData.gridSellToday = inverterData.gridSellTotal - gridSellTotal - (inverterData.loadTotal - loadTotal);
+                            else
+                            {
+                                log_d("Invalid packet size: %d", len);
+                            }
+                        }
+                        else
+                        {
+                            log_d("CRC error");
                         }
                     }
+                    else
+                    {
+                        log_d("No data received");
+                    }
                 }
+
+                udp.clear();
             }
 
-            for(int i = 0; i < 3; i++) { //it is UDP so retries are needed
+            for (int i = 0; i < 3; i++)
+            { // it is UDP so retries are needed
                 sendBMSInfoRequestPacket();
                 if (awaitPacket(3000))
                 {
@@ -211,11 +229,10 @@ private:
                         }
                     }
                 }
+                udp.clear();
             }
         }
         disconnect();
-
-        logInverterData(inverterData);
         return inverterData;
     }
 };
