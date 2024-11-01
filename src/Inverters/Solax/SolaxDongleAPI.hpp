34494fd3b7a89015eea6998fb6cea38ff6f12df4
Lukas Jezny <ljezny@gmail.com>
2024-11-01 17:39:51 +0100

    Solax Yield fix

--- src/Inverters/Solax/SolaxDongleAPI.hpp
+++ src/Inverters/Solax/SolaxDongleAPI.hpp
@@ -61,7 +61,7 @@ public:
                                     inverterData.inverterPower = ((int16_t) doc["Data"][9].as<uint16_t>());
                                     inverterData.loadPower = read16BitSigned(doc["Data"][47].as<uint16_t>());
                                     inverterData.soc = doc["Data"][103].as<int>();
-                                    inverterData.pvToday = doc["Data"][70].as<uint16_t>() / 10.0;
+                                    inverterData.pvToday = doc["Data"][70].as<uint16_t>() / 10.0 - doc["Data"][78].as<uint16_t>() / 10.0; //yield is PV + battery
                                     inverterData.pvTotal = ((doc["Data"][69].as<uint32_t>() << 16) + doc["Data"][68].as<uint16_t>()) / 10.0;
                                     inverterData.feedInPower = read16BitSigned(doc["Data"][34].as<uint16_t>());
                                     inverterData.gridSellToday = doc["Data"][90].as<uint16_t>() / 100.0; 
