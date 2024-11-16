e4537ad6bf4b8621499430b58285c9b32480d138
Lukas Jezny <ljezny@gmail.com>
2024-11-16 09:23:51 +0100

    Sofar refactoring

--- src/Inverters/SofarSolar/SofarSolarDongleAPI.hpp
+++ src/Inverters/SofarSolar/SofarSolarDongleAPI.hpp
@@ -17,9 +17,7 @@ public:
 private:
     WiFiClient client;
 
-    double gridBuyTotal = 0;
-    double gridSellTotal = 0;
-    int day = -1;
+    uint16_t sequenceNumber = 0;
 
     uint16_t readUInt16(byte *buf, byte reg)
     {
@@ -57,7 +55,7 @@ private:
         client.stop();
     }
 
-    bool sendReadDataRequest(uint16_t requestNo, uint16_t addr, uint8_t len, uint32_t sn)
+    bool sendReadDataRequest(uint16_t sequenceNumber, uint16_t addr, uint8_t len, uint32_t sn)
     {
         byte solarmanv5Header[] = {
             0xA5 /*Start of packet*/,
@@ -65,8 +63,8 @@ private:
             0x00 /*Packet length*/,
             0x45 /*Packet type, request*/,
             0x10 /*Packet type, request*/,
-            requestNo & 0xff /*Request number*/,
-            requestNo >> 8 /*Request number*/,
+            sequenceNumber & 0xff /*Request number*/,
+            sequenceNumber >> 8 /*Request number*/,
             sn & 0xff /*Serial number*/,
             (sn >> 8) & 0xff /*Serial number*/,
             (sn >> 16) & 0xff /*Serial number*/,
@@ -152,30 +150,31 @@ private:
         return true;
     }
 
-    bool sendRunningDataRequestPacket()
+    bool sendRunningDataRequestPacket(uint32_t sn)
     {
-        return sendReadDataRequest(0x0200 , 69);
+        return sendReadDataRequest(sequenceNumber, 0x0 , 0x0F, sn);
     }
 
     bool awaitPacket(int timeout)
     {
-        return client.available() > 0;
-        // unsigned long start = millis();
-        // while (millis() - start < timeout)
-        // {
-        //     int packetSize = udp.parsePacket();
-        //     if (packetSize)
-        //     {
-        //         return true;
-        //     }
-        // }
-        // return false;
+        unsigned long start = millis();
+        while (millis() - start < timeout)
+        {
+            int packetSize = client.available();
+            if (packetSize)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
-    InverterData_t readData(String sn)
+    InverterData_t readData(String dongleSN)
     {
         InverterData_t inverterData;
         log_d("Connecting to dongle...");
+        uint16_t sn = strtoul(dongleSN.c_str(), NULL, 10);
+        log_d("SN: %d", sn);
         if (connect())
         {
             log_d("Connected.");
@@ -184,55 +183,35 @@ private:
 
             for (int i = 0; i < 3; i++)
             {
-                sendRunningDataRequestPacket();
+                sendRunningDataRequestPacket(sn);
+                sequenceNumber++;
                 if (awaitPacket(3000))
                 {
                     int len = client.read(packetBuffer, PACKET_SIZE);
                     if (len > 0)
                     {
-                        unsigned c = crc16(packetBuffer + 2, len - 2, 0x8005, 0xFFFF, 0, true, true);
-                        if (c == 0)
-                        {
-                            if (len == 7 + 2 * 69)
-                            {
-                                inverterData.status = DONGLE_STATUS_OK;
-                                inverterData.millis = millis();
-                                inverterData.pv1Power = readUInt32(packetBuffer, 0x14);
-                                
-                                // inverterData.inverterPower = readInt16(packetBuffer, 38);
-                                // inverterData.batteryPower -= readInt16(packetBuffer, 83); // TODO: maybe sign readuw(packetBuffer, 84);
-                                // // _ac = readsw(packetBuffer, 40);
-                                // inverterData.L1Power = readInt16(packetBuffer, 25); // - readInt16(packetBuffer, 64) + readInt16(packetBuffer, 50);
-                                // inverterData.L2Power = readInt16(packetBuffer, 30); // - readInt16(packetBuffer, 66) + readInt16(packetBuffer, 56);
-                                // inverterData.L3Power = readInt16(packetBuffer, 35); // - readInt16(packetBuffer, 68) + readInt16(packetBuffer, 62);
-                                // inverterData.feedInPower =
-                                //     readInt16(packetBuffer, 25) + readInt16(packetBuffer, 30) + readInt16(packetBuffer, 35) - readInt16(packetBuffer, 64) - readInt16(packetBuffer, 50) - readInt16(packetBuffer, 66) - readInt16(packetBuffer, 56) - readInt16(packetBuffer, 68) - readInt16(packetBuffer, 62);
-                                // inverterData.loadPower = readInt16(packetBuffer, 72) + readInt16(packetBuffer, 70);
-                                // inverterData.inverterTemperature = readInt16(packetBuffer, 74) / 10;
-                                // inverterData.pvTotal = readUInt32(packetBuffer, 91) / 10.0;
-                                // inverterData.pvToday = readUInt32(packetBuffer, 93) / 10.0;
-                                // inverterData.loadToday = readUInt16(packetBuffer, 105) / 10.0;
-                                // inverterData.loadTotal = readUInt32(packetBuffer, 103) / 10.0;
-                                // inverterData.batteryChargedToday = readUInt16(packetBuffer, 108) / 10.0;
-                                // inverterData.batteryDischargedToday = readUInt16(packetBuffer, 111) / 10.0;
-                                inverterData.sn = sn;
-                                logInverterData(inverterData);
-
-                                break;
-                            }
-                            else
-                            {
-                                log_d("Invalid packet size: %d", len);
-                            }
-                        }
-                        else
-                        {
-                            log_d("CRC error");
-                        }
-                    }
-                    else
-                    {
-                        log_d("No data received");
+                        inverterData.status = DONGLE_STATUS_OK;
+                        inverterData.millis = millis();
+                        //inverterData.pv1Power = readUInt32(packetBuffer, 0x14);
+                        
+                        // inverterData.inverterPower = readInt16(packetBuffer, 38);
+                        // inverterData.batteryPower -= readInt16(packetBuffer, 83); // TODO: maybe sign readuw(packetBuffer, 84);
+                        // // _ac = readsw(packetBuffer, 40);
+                        // inverterData.L1Power = readInt16(packetBuffer, 25); // - readInt16(packetBuffer, 64) + readInt16(packetBuffer, 50);
+                        // inverterData.L2Power = readInt16(packetBuffer, 30); // - readInt16(packetBuffer, 66) + readInt16(packetBuffer, 56);
+                        // inverterData.L3Power = readInt16(packetBuffer, 35); // - readInt16(packetBuffer, 68) + readInt16(packetBuffer, 62);
+                        // inverterData.feedInPower =
+                        //     readInt16(packetBuffer, 25) + readInt16(packetBuffer, 30) + readInt16(packetBuffer, 35) - readInt16(packetBuffer, 64) - readInt16(packetBuffer, 50) - readInt16(packetBuffer, 66) - readInt16(packetBuffer, 56) - readInt16(packetBuffer, 68) - readInt16(packetBuffer, 62);
+                        // inverterData.loadPower = readInt16(packetBuffer, 72) + readInt16(packetBuffer, 70);
+                        // inverterData.inverterTemperature = readInt16(packetBuffer, 74) / 10;
+                        // inverterData.pvTotal = readUInt32(packetBuffer, 91) / 10.0;
+                        // inverterData.pvToday = readUInt32(packetBuffer, 93) / 10.0;
+                        // inverterData.loadToday = readUInt16(packetBuffer, 105) / 10.0;
+                        // inverterData.loadTotal = readUInt32(packetBuffer, 103) / 10.0;
+                        // inverterData.batteryChargedToday = readUInt16(packetBuffer, 108) / 10.0;
+                        // inverterData.batteryDischargedToday = readUInt16(packetBuffer, 111) / 10.0;
+                        inverterData.sn = sn;
+                        logInverterData(inverterData);
                     }
                 }
             }
