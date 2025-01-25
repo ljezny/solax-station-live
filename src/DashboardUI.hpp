59e5e2adc00efc84f1c0900e68cb389de6e39b6f
Lukas Jezny <ljezny@gmail.com>
2025-01-25 13:41:37 +0100

    Removes Now

--- src/DashboardUI.hpp
+++ src/DashboardUI.hpp
@@ -77,7 +77,7 @@ static void draw_event_cb(lv_event_t *e)
             int hours = totalMinutes / 60;
             int minutes = totalMinutes % 60;
             if(totalMinutes == 0) {
-                lv_snprintf(dsc->text, dsc->text_length, "Now");
+                lv_snprintf(dsc->text, dsc->text_length, "");
             } else {
                 String format = "-";
                 if(hours > 0) {
