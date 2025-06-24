2b875c4a642bc7d0d9ca65a77c213184efaa6b1f
Lukas Jezny <ljezny@gmail.com>
2025-06-24 15:16:04 +0200

    Deye HV support

--- src/Inverters/Deye/DeyeDongleAPI.hpp
+++ src/Inverters/Deye/DeyeDongleAPI.hpp
@@ -11,6 +11,16 @@ public:
     }
 
 private:
+    uint16_t readUInt16_H10(byte *buf, byte reg)
+    {
+        return ((buf[3 + reg * 2] << 8) * 10) | buf[3 + reg * 2 + 1];
+    }
+
+    int16_t readInt16_H10(byte *buf, byte reg)
+    {
+        return ((buf[3 + reg * 2] << 8) * 10) | buf[3 + reg * 2 + 1];
+    }
+
     InverterData_t readData(String dongleSN)
     {
         InverterData_t inverterData;
@@ -23,18 +33,48 @@ private:
             byte packetBuffer[1024];
 
             inverterData.sn = sn;
-
-            // pv input
-            // 672-673
-            // but we need only few
-            if (sendReadDataRequest(client, sequenceNumber, 672, 673 - 672 + 1, sn))
+            bool isV104 = false;
+            if (sendReadDataRequest(client, sequenceNumber, 0, 8 - 0 + 1, sn))
             {
                 if (readModbusRTUResponse(client, packetBuffer, sizeof(packetBuffer)) > 0)
                 {
                     inverterData.status = DONGLE_STATUS_OK;
                     inverterData.millis = millis();
-                    inverterData.pv1Power = readUInt16(packetBuffer, 672 - 672);
-                    inverterData.pv2Power = readUInt16(packetBuffer, 673 - 672);
+                    uint16_t deviceType = readUInt16(packetBuffer, 0);
+                    log_d("Device type: %s", String(deviceType, HEX));
+                    uint16_t commProtoVer = readUInt16(packetBuffer, 2);
+                    log_d("Comm protocol version: %s", String(commProtoVer, HEX));
+                    isV104 = (commProtoVer >= 0x0104);
+                    String inverterSN = readString(packetBuffer, 3, 10);
+                    log_d("Inverter SN: %s", inverterSN.c_str());
+                    inverterData.sn = inverterSN;
+                }
+                else
+                {
+                    disconnect(client);
+                    inverterData.status = DONGLE_STATUS_CONNECTION_ERROR;
+                    return inverterData;
+                }
+            }
+            else
+            {
+                log_d("Failed to send request");
+                disconnect(client);
+                inverterData.status = DONGLE_STATUS_CONNECTION_ERROR;
+                return inverterData;
+            }
+
+            // pv input
+            // 672-673
+            // but we need only few
+            if (sendReadDataRequest(client, sequenceNumber, 672, 675 - 672 + 1, sn))
+            {
+                if (readModbusRTUResponse(client, packetBuffer, sizeof(packetBuffer)) > 0)
+                {
+                    inverterData.pv1Power = isV104 ? readUInt16_H10(packetBuffer, 672 - 672) : readUInt16(packetBuffer, 672 - 672);
+                    inverterData.pv2Power = isV104 ? readUInt16_H10(packetBuffer, 673 - 672) : readUInt16(packetBuffer, 673 - 672);
+                    inverterData.pv3Power = isV104 ? readUInt16_H10(packetBuffer, 674 - 672) : readUInt16(packetBuffer, 674 - 672);
+                    inverterData.pv4Power = isV104 ? readUInt16_H10(packetBuffer, 675 - 672) : readUInt16(packetBuffer, 675 - 672);
                 }
                 else
                 {
@@ -59,7 +99,7 @@ private:
                 {
                     inverterData.batteryTemperature = (readInt16(packetBuffer, 586 - 586)  - 1000)  / 10;
                     inverterData.soc = readUInt16(packetBuffer, 588 - 586);
-                    inverterData.batteryPower = -1 * readInt16(packetBuffer, 590 - 586); //Battery power flow - negative for charging, positive for discharging
+                    inverterData.batteryPower = isV104 ? -1 * readInt16_H10(packetBuffer, 590 - 586) : -1 * readInt16(packetBuffer, 590 - 586); //Battery power flow - negative for charging, positive for discharging
                 }
                 else
                 {
